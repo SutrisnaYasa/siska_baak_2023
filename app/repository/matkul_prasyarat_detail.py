c81aabba938d6cc26926528acdfa38cfb8c769c6
SutrisnaYasa <sutrisnayasa6@gmail.com>
2023-06-09 14:52:17 +0800

    response matkul prasyarat detail

--- app/repository/matkul_prasyarat_detail.py
+++ app/repository/matkul_prasyarat_detail.py
@@ -1,7 +1,9 @@
 from sqlalchemy.orm import Session
 import models, schemas
-from fastapi import HTTPException, status
+from fastapi import HTTPException, status, Response
 from typing import List, Dict, Union
+import json
+from sqlalchemy import exists
 
 def get_all(db: Session) -> Dict[str, Union[bool, str, schemas.ShowMatkulPrasyarat]]:
     response = {"status": False, "message": "", "data": []}
@@ -15,7 +17,7 @@ def get_all(db: Session) -> Dict[str, Union[bool, str, schemas.ShowMatkulPrasyar
             response["message"] = "Data Matkul Prasyarat Detail Masih Kosong"
     except Exception as e:
         response["message"] = str(e)
-    return response
+    return {"detail": [response]}
 
 def create(request: schemas.MatkulPrasyaratDetail, db: Session) -> Dict[str, Union[bool, str, schemas.ShowMatkulPrasyaratDetail]]:
     response = {"status": False, "message": "", "data": None}
@@ -29,13 +31,20 @@ def create(request: schemas.MatkulPrasyaratDetail, db: Session) -> Dict[str, Uni
         response["data"] = schemas.ShowMatkulPrasyaratDetail.from_orm(new_matkul_prasyarat_detail)
     except Exception as e:
         response["message"] = str(e)
-    return response
+    return {"detail": [response]}
 
 def destroy(id: int, db: Session) -> Dict[str, Union[bool, str]]:
     response = {"status": False, "message": ""}
     matkul_prasyarat_detail = db.query(models.MatkulPrasyaratDetail).filter(models.MatkulPrasyaratDetail.id == id)
     if not matkul_prasyarat_detail.first():
-        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = f"Matkul Prasyarat Detail dengan id {id} tidak ditemukan")
+        response["msg"] = f"Data Fakultas dengan id {id} tidak ditemukan"
+        content = json.dumps({"detail":[response]})
+        return Response(
+            content = content, 
+            media_type = "application/json", 
+            status_code = status.HTTP_404_NOT_FOUND, 
+            headers = {"X-Error": "Data Matkul Prasyarat Detail tidak ditemukan"}
+        )
     try:
         matkul_prasyarat_detail.delete(synchronize_session = False)
         db.commit()
@@ -43,13 +52,20 @@ def destroy(id: int, db: Session) -> Dict[str, Union[bool, str]]:
         response["message"] = "Data Matkul Prasyarat Detail Berhasil di Hapus"
     except Exception as e:
         response["message"] = str(e)
-    return response
+    return {"detail": [response]}
 
 def update(id: int, request: schemas.MatkulPrasyaratDetail, db: Session) -> Dict[str, Union[bool, str, schemas.ShowMatkulPrasyaratDetail]]:
     response = {"status": False, "message": "", "data": None}
     matkul_prasyarat_detail = db.query(models.MatkulPrasyaratDetail).filter(models.MatkulPrasyaratDetail.id == id)
     if not matkul_prasyarat_detail.first():
-        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = f"Matkul Prasyarat Detail dengan id {id} tidak ditemukan")
+        response["msg"] = f"Data Fakultas dengan id {id} tidak ditemukan"
+        content = json.dumps({"detail":[response]})
+        return Response(
+            content = content, 
+            media_type = "application/json", 
+            status_code = status.HTTP_404_NOT_FOUND, 
+            headers = {"X-Error": "Data Matkul Prasyarat Detail tidak ditemukan"}
+        )
     try:
         matkul_prasyarat_detail.update(request.dict())
         db.commit()
@@ -59,17 +75,24 @@ def update(id: int, request: schemas.MatkulPrasyaratDetail, db: Session) -> Dict
         raise HTTPException(status_code = status.HTTP_422_UNPROCESSABLE_ENTITY, detail = str(ve))
     except Exception as e:
         response["message"] = str(e)
-    return response
+    return {"detail": [response]}
 
 def show(id: int, db: Session) -> Dict[str, Union[bool, str, schemas.ShowMatkulPrasyaratDetail]]:
     response = {"status": False, "message": "", "data": None}
     matkul_prasyarat_detail = db.query(models.MatkulPrasyaratDetail).filter(models.MatkulPrasyaratDetail.id == id).first()
     if not matkul_prasyarat_detail:
-        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = f"Matkul Prasyarat Detail dengan id {id} tidak ditemukan")
+        response["msg"] = f"Data Fakultas dengan id {id} tidak ditemukan"
+        content = json.dumps({"detail":[response]})
+        return Response(
+            content = content, 
+            media_type = "application/json", 
+            status_code = status.HTTP_404_NOT_FOUND, 
+            headers = {"X-Error": "Data Matkul Prasyarat Detail tidak ditemukan"}
+        )
     try:
         response["status"] = True
         response["message"] = "Data Matkul Prasyarat Detail Berhasil Ditemukan"
         response["data"] = schemas.ShowMatkulPrasyaratDetail.from_orm(matkul_prasyarat_detail)
     except Exception as e:
         response["message"] = str(e)
-    return response
+    return {"detail": [response]}
