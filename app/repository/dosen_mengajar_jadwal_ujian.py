89495d51ccc20bb9ba542fd04bf6e3a49cf02a41
SutrisnaYasa <sutrisnayasa6@gmail.com>
2023-06-09 15:40:15 +0800

    response dosen mengajar jadwal ujian

--- app/repository/dosen_mengajar_jadwal_ujian.py
+++ app/repository/dosen_mengajar_jadwal_ujian.py
@@ -1,78 +1,101 @@
 from sqlalchemy.orm import Session
 import models, schemas
-from fastapi import HTTPException, status
+from fastapi import HTTPException, status, Response
 from typing import List, Dict, Union
+import json
+from sqlalchemy import exists
 
 def get_all(db: Session) -> Dict[str, Union[bool, str, schemas.ShowDosenMengajarJadwalUjian]]:
-    response = {"status": False, "message": "", "data": []}
+    response = {"status": False, "msg": "", "data": []}
     try:
         dosen_mengajar_jadwal_ujian_all = db.query(models.DosenMengajarJadwalUjian).all()
         if dosen_mengajar_jadwal_ujian_all:
             response["status"] = True
-            response["message"] = "Data Jadwal Ujian Dosen Mengajar Berhasil Ditemukan"
+            response["msg"] = "Data Jadwal Ujian Dosen Mengajar Berhasil Ditemukan"
             response["data"] = dosen_mengajar_jadwal_ujian_all
         else:
-            response["message"] = "Data Jadwal Ujian Dosen Mengajar Masih Kosong"
+            response["msg"] = "Data Jadwal Ujian Dosen Mengajar Masih Kosong"
     except Exception as e:
-        response["message"] = str(e)
-    return response
+        response["msg"] = str(e)
+    return {"detail": [response]}
 
 def create(request: schemas.DosenMengajarJadwalUjian, db: Session) -> Dict[str, Union[bool, str, schemas.ShowDosenMengajarJadwalUjian]]:
-    response = {"status": False, "message": "", "data": None}
+    response = {"status": False, "msg": "", "data": None}
     try:
         new_dosen_mengajar_jadwal_ujian = models.DosenMengajarJadwalUjian(** request.dict())
         db.add(new_dosen_mengajar_jadwal_ujian)
         db.commit()
         db.refresh(new_dosen_mengajar_jadwal_ujian)
         response["status"] = True
-        response["message"] = "Data Jadwal Ujian Dosen Mengajar Berhasil di Input"
+        response["msg"] = "Data Jadwal Ujian Dosen Mengajar Berhasil di Input"
         response["data"] = schemas.ShowDosenMengajarJadwalUjian.from_orm(new_dosen_mengajar_jadwal_ujian)
     except ValueError as ve:
         raise HTTPException(status_code = status.HTTP_422_UNPROCESSABLE_ENTITY, detail = str(ve))
     except Exception as e:
-        response["message"] = str(e)
-    return response
+        response["msg"] = str(e)
+    return {"detail": [response]}
 
 def destroy(id: int, db: Session) -> Dict[str, Union[bool, str]]:
-    response = {"status": False, "message": ""}
+    response = {"status": False, "msg": ""}
     dosen_mengajar_jadwal_ujian = db.query(models.DosenMengajarJadwalUjian).filter(models.DosenMengajarJadwalUjian.id == id)
     if not dosen_mengajar_jadwal_ujian.first():
-        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = f"Data Jadwal Ujian dengan id {id} tidak ditemukan")
+        response["msg"] = f"Data Jadwal Ujian Dosen Mengajar dengan id {id} tidak ditemukan"
+        content = json.dumps({"detail":[response]})
+        return Response(
+            content = content, 
+            media_type = "application/json", 
+            status_code = status.HTTP_404_NOT_FOUND, 
+            headers = {"X-Error": "Data Jadwal Ujian Dosen Mengajar tidak ditemukan"}
+        )
     try:
         dosen_mengajar_jadwal_ujian.delete(synchronize_session = False)
         db.commit()
         response["status"] = True
-        response["message"] = "Data Jadwal Ujian Dosen Mengajar Berhasil di Hapus"
+        response["msg"] = "Data Jadwal Ujian Dosen Mengajar Berhasil di Hapus"
     except Exception as e:
-        response["message"] = str(e)
-    return response
+        response["msg"] = str(e)
+    return {"detail": [response]}
 
 def update(id: int, request: schemas.DosenMengajarJadwalUjian, db: Session) -> Dict[str, Union[bool, str, schemas.ShowDosenMengajarJadwalUjian]]:
-    response = {"status": False, "message": "", "data": None}
+    response = {"status": False, "msg": "", "data": None}
     dosen_mengajar_jadwal_ujian = db.query(models.DosenMengajarJadwalUjian).filter(models.DosenMengajarJadwalUjian.id == id)
     if not dosen_mengajar_jadwal_ujian.first():
-        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = f"Data Jadwal Ujian dengan id {id} tidak ditemukan")
+        response["msg"] = f"Data Jadwal Ujian Dosen Mengajar dengan id {id} tidak ditemukan"
+        content = json.dumps({"detail":[response]})
+        return Response(
+            content = content, 
+            media_type = "application/json", 
+            status_code = status.HTTP_404_NOT_FOUND, 
+            headers = {"X-Error": "Data Jadwal Ujian Dosen Mengajar tidak ditemukan"}
+        )
     try:
         dosen_mengajar_jadwal_ujian.update(request.dict())
         db.commit()
         response["status"] = True
-        response["message"] = "Data Jadwal Ujian Dosen Mengajar Berhasil di Update"
+        response["msg"] = "Data Jadwal Ujian Dosen Mengajar Berhasil di Update"
         response["data"] = schemas.ShowDosenMengajarJadwalUjian.from_orm(dosen_mengajar_jadwal_ujian.first())
     except ValueError as ve:
         raise HTTPException(status_code = status.HTTP_422_UNPROCESSABLE_ENTITY, detail = str(ve))
     except Exception as e:
-        response["message"] = str(e)
-    return response
+        response["msg"] = str(e)
+    return {"detail": [response]}
 
 def show(id: int, db: Session) -> Dict[str, Union[bool, str, schemas.ShowDosenMengajarJadwalUjian]]:
-    response = {"status": False, "message": "", "data": None}
+    response = {"status": False, "msg": "", "data": None}
     dosen_mengajar_jadwal_ujian = db.query(models.DosenMengajarJadwalUjian).filter(models.DosenMengajarJadwalUjian.id == id).first()
     if not dosen_mengajar_jadwal_ujian:
-        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = f"Data Jadwal Ujian dengan id {id} tidak ditemukan")
+        response["msg"] = f"Data Jadwal Ujian Dosen Mengajar dengan id {id} tidak ditemukan"
+        content = json.dumps({"detail":[response]})
+        return Response(
+            content = content, 
+            media_type = "application/json", 
+            status_code = status.HTTP_404_NOT_FOUND, 
+            headers = {"X-Error": "Data Jadwal Ujian Dosen Mengajar tidak ditemukan"}
+        )
     try:
         response["status"] = True
-        response["message"] = "Data Jadwal Ujian Dosen Mengajar Berhasil Ditemukan"
+        response["msg"] = "Data Jadwal Ujian Dosen Mengajar Berhasil Ditemukan"
         response["data"] = schemas.ShowDosenMengajarJadwalUjian.from_orm(dosen_mengajar_jadwal_ujian)
     except Exception as e:
-        response["message"] = str(e)
-    return response
+        response["msg"] = str(e)
+    return {"detail": [response]}
