4475b7108a8fde671af985b559503543b8fc5c02
SutrisnaYasa <sutrisnayasa6@gmail.com>
2023-06-12 15:45:39 +0800

    menambahkan deleted_at di kurikulum

--- app/repository/kurikulum.py
+++ app/repository/kurikulum.py
@@ -3,12 +3,13 @@ import models, schemas
 from fastapi import HTTPException, status, Response
 from typing import List, Dict, Union
 import json
-from sqlalchemy import exists
+from sqlalchemy import exists, and_
+import datetime
 
 def get_all(db: Session) -> Dict[str, Union[bool, str, schemas.ShowKurikulum]]:
     response = {"status": False, "msg": "", "data": []}
     try:
-        kurikulum_all = db.query(models.Kurikulum).all()
+        kurikulum_all = db.query(models.Kurikulum).filter(models.Kurikulum.deleted_at == None).all()
         if kurikulum_all:
             response["status"] = True
             response["msg"] = "Data Kurikulum Berhasil Ditemukan"
@@ -37,18 +38,25 @@ def create(request: schemas.Kurikulum, db: Session) -> Dict[str, Union[bool, str
 
 def destroy(id: int, db: Session) -> Dict[str, Union[bool, str]]:
     response = {"status": False, "msg": ""}
-    kurikulum = db.query(models.Kurikulum).filter(models.Kurikulum.id == id)
-    if not kurikulum.first():
-        response["msg"] = f"Data Kurikulum dengan id {id} tidak ditemukan"
+    kurikulum = db.query(models.Kurikulum).filter(models.Kurikulum.id == id, models.Kurikulum.deleted_at.is_(None))
+
+    existing_kurikulum = kurikulum.first()
+    if not existing_kurikulum:
+        if db.query(models.Kurikulum).filter(models.Kurikulum.id == id).first():
+            response["msg"] = f"Data Kurikulum dengan id {id} sudah dihapus"
+            status_code = status.HTTP_400_BAD_REQUEST
+        else:
+            response["msg"] = f"Data Kurikulum dengan id {id} tidak ditemukan"
+            status_code = status.HTTP_404_NOT_FOUND
         content = json.dumps({"detail": [response]})
         return Response(
             content = content,
             media_type = "application/json",
-            status_code = status.HTTP_404_NOT_FOUND,
-            headers = {"X-Error": "Data Kurikulum tidak ditemukan"}
+            status_code = status_code,
+            headers = {"X-Error": response["msg"]}
         )
     try:
-        kurikulum.delete(synchronize_session = False)
+        kurikulum.update({models.Kurikulum.deleted_at: datetime.datetime.now()})
         db.commit()
         response["status"] = True
         response["msg"] = "Data Kurikulum Berhasil di Hapus"
@@ -66,6 +74,15 @@ def update(id: int, request: schemas.Kurikulum, db: Session) -> Dict[str, Union[
             content = content,
             media_type = "application/json",
             status_code = status.HTTP_404_NOT_FOUND,
+            headers = {"X-Error": "Data Kurikulum tidak ditemukan"}
+        )
+    if kurikulum.first().deleted_at:
+        response["msg"] = f"Data Kurikulum dengan id {id} telah dihapus"
+        content = json.dumps({"detail": [response]})
+        return Response(
+            content = content,
+            media_type = "application/json",
+            status_code = status.HTTP_400_BAD_REQUEST,
             headers = {"X-Error": "Data Kurikulum tidak ditemukan"}
         )
     try:
@@ -90,6 +107,15 @@ def show(id: int, db: Session) -> Dict[str, Union[bool, str, schemas.ShowKurikul
             content = content,
             media_type = "application/json",
             status_code = status.HTTP_404_NOT_FOUND,
+            headers = {"X-Error": "Data Kurikulum tidak ditemukan"}
+        )
+    if kurikulum.deleted_at:
+        response["msg"] = f"Data Kurikulum dengan id {id} telah dihapus"
+        content = json.dumps({"detail": [response]})
+        return Response(
+            content = content,
+            media_type = "application/json",
+            status_code = status.HTTP_400_BAD_REQUEST,
             headers = {"X-Error": "Data Kurikulum tidak ditemukan"}
         )
     try:
