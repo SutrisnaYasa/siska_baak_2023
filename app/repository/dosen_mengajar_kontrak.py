6b980aefe9898883524eb156e207ea01404259f2
SutrisnaYasa <sutrisnayasa6@gmail.com>
2023-06-13 10:22:12 +0800

    menambahkan deleted_at di dosen mengajar kontrak

--- app/repository/dosen_mengajar_kontrak.py
+++ app/repository/dosen_mengajar_kontrak.py
@@ -3,12 +3,13 @@ import models, schemas
 from fastapi import HTTPException, status, Response
 from typing import List, Dict, Union
 import json
-from sqlalchemy import exists
+from sqlalchemy import exists, and_
+import datetime
 
 def get_all(db: Session) -> Dict[str, Union[bool, str, schemas.ShowDosenMengajarKontrak]]:
     response = {"status": False, "msg": "", "data": []}
     try:
-        dosen_mengajar_kontrak_all = db.query(models.DosenMengajarKontrak).all()
+        dosen_mengajar_kontrak_all = db.query(models.DosenMengajarKontrak).filter(models.DosenMengajarKontrak.deleted_at == None).all()
         if dosen_mengajar_kontrak_all:
             response["status"] = True
             response["msg"] = "Data Kontrak Mengajar Dosen Berhasil Ditemukan"
@@ -37,18 +38,25 @@ def create(request: schemas.DosenMengajarKontrak, db: Session) -> Dict[str, Unio
 
 def destroy(id: int, db: Session) -> Dict[str, Union[bool, str]]:
     response = {"status": False, "msg": ""}
-    dosen_mengajar_kontrak = db.query(models.DosenMengajarKontrak).filter(models.DosenMengajarKontrak.id == id)
-    if not dosen_mengajar_kontrak.first():
-        response["msg"] = f"Data Kontrak Mengajar Dosen dengan id {id} tidak ditemukan"
-        content = json.dumps({"detail":[response]})
+    dosen_mengajar_kontrak = db.query(models.DosenMengajarKontrak).filter(models.DosenMengajarKontrak.id == id, models.DosenMengajarKontrak.deleted_at.is_(None))
+
+    existing_dosen_mengajar_kontrak = dosen_mengajar_kontrak.first()
+    if not existing_dosen_mengajar_kontrak:
+        if db.query(models.DosenMengajarKontrak).filter(models.DosenMengajarKontrak.id == id).first():
+            response["msg"] = f"Data Kontrak Mengajar Dosen dengan id {id} sudah dihapus"
+            status_code = status.HTTP_400_BAD_REQUEST
+        else:
+            response["msg"] = f"Data Kontrak Mengajar Dosen dengan id {id} tidak ditemukan"
+            status_code = status.HTTP_404_NOT_FOUND
+        content = json.dumps({"detail": [response]})
         return Response(
-            content = content, 
-            media_type = "application/json", 
-            status_code = status.HTTP_404_NOT_FOUND, 
-            headers = {"X-Error": "Data Kontrak Mengajar Dosen tidak ditemukan"}
-        )
+            content = content,
+            media_type = "application/json",
+            status_code = status_code,
+            headers = {"X-Error": response["msg"]}
+       )
     try:
-        dosen_mengajar_kontrak.delete(synchronize_session = False)
+        dosen_mengajar_kontrak.update({models.DosenMengajarKontrak.deleted_at: datetime.datetime.now()})
         db.commit()
         response["status"] = True
         response["msg"] = "Data Kontrak Mengajar Dosen Berhasil di Hapus"
@@ -67,6 +75,15 @@ def update(id: int, request: schemas.DosenMengajarKontrak, db: Session) -> Dict[
             media_type = "application/json", 
             status_code = status.HTTP_404_NOT_FOUND, 
             headers = {"X-Error": "Data Kontrak Mengajar Dosen tidak ditemukan"}
+        )
+    if dosen_mengajar_kontrak.first().deleted_at:
+        response["msg"] = f"Data Kontrak Mengajar Dosen dengan id {id} sudah dihapus"
+        content = json.dumps({"detail": [response]})
+        return Response(
+            content = content,
+            media_type = "application/json",
+            status_code = status.HTTP_400_BAD_REQUEST,
+            headers = {"X-Error": "Data Kontrak Mengajar Dosen sudah dihapus"}
         )
     try:
         dosen_mengajar_kontrak.update(request.dict())
@@ -91,6 +108,15 @@ def show(id: int, db: Session) -> Dict[str, Union[bool, str, schemas.ShowDosenMe
             media_type = "application/json", 
             status_code = status.HTTP_404_NOT_FOUND, 
             headers = {"X-Error": "Data Kontrak Mengajar Dosen tidak ditemukan"}
+        )
+    if dosen_mengajar_kontrak.deleted_at:
+        response["msg"] = f"Data Kontrak Mengajar Dosen dengan id {id} sudah dihapus"
+        content = json.dumps({"detail": [response]})
+        return Response(
+            content = content,
+            media_type = "application/json",
+            status_code = status.HTTP_400_BAD_REQUEST,
+            headers = {"X-Error": "Data Kontrak Mengajar Dosen sudah dihapus"}
         )
     try:
         response["status"] = True
