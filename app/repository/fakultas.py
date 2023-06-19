927db67b99f05b33e8de84a124fb4f6e21b15130
SutrisnaYasa <sutrisnayasa6@gmail.com>
2023-06-19 14:07:18 +0800

    revisi update fakultas

--- app/repository/fakultas.py
+++ app/repository/fakultas.py
@@ -106,16 +106,6 @@ def update(id: int, request: schemas.Fakultas, db: Session) -> Dict[str, Union[b
             headers = {"X-Error": "Data Fakultas tidak ditemukan"}
         )
     
-    existing_fakultas = db.query(models.Fakultas).filter(models.Fakultas.kode_fakultas == request.kode_fakultas).first()
-    if existing_fakultas and existing_fakultas.id_fakultas != id:
-        response["msg"] = "Kode Fakultas Sudah Ada"
-        content = json.dumps({"detail":[response]})
-        return Response(
-            content = content, 
-            media_type = "application/json", 
-            status_code = status.HTTP_409_CONFLICT, 
-            headers = {"X-Error": "Data Conflict"}
-        )
     if fakultas.first().deleted_at:
         response["msg"] = f"Data Fakultas dengan id {id} telah dihapus"
         content = json.dumps({"detail": [response]})
@@ -124,6 +114,21 @@ def update(id: int, request: schemas.Fakultas, db: Session) -> Dict[str, Union[b
             media_type = "application/json",
             status_code = status.HTTP_400_BAD_REQUEST,
             headers = {"X-Error": "Data Fakultas telah dihapus"}
+        )
+    existing_fakultas = db.query(models.Fakultas).filter(
+        models.Fakultas.kode_fakultas == request.kode_fakultas,
+        models.Fakultas.deleted_at.is_(None),
+        models.Fakultas.id_fakultas != id
+    ).first()
+    
+    if existing_fakultas:
+        response["msg"] = "Kode Fakultas Sudah Ada"
+        content = json.dumps({"detail": [response]})
+        return Response(
+            content=content,
+            media_type="application/json",
+            status_code=status.HTTP_409_CONFLICT,
+            headers={"X-Error": "Data Conflict"}
         )
     try:
         fakultas.update(request.dict())
